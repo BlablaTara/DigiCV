e1abdbc009a2d328819a93ce80b285261a9e91e4
BlablaTara <tara0561@stud.kea.dk>
2026-03-12 00:35:45 +0100

    added anchored navigation

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <div>
+            <h1>Om mig</h1>
+        </div>
+    );
+}
+
+// function About() = react komponent
+// return() = HTML for show
+// 'export default' = makes the component to a page
