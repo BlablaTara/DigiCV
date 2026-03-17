14341253bbdac74336cde05a9b2895ed48bd24e7
BlablaTara <tara0561@stud.kea.dk>
2026-03-17 17:32:39 +0100

    added burger-bar to X

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav>
             <div className="nav-container">
+
                 <div className="logo">Emma Lykke</div>
 
-                <div className="nav-links">
-                    <a href="#home">Forside</a>
-                    <a href="#projects">Projekter</a>
-                    <a href="#about">Om mig</a>
+                <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+                    <a href="#home" onClick={() => setMenuOpen(false)}>Forside</a>
+                    <a href="#projects" onClick={() => setMenuOpen(false)}>Projekter</a>
+                    <a href="#about" onClick={() => setMenuOpen(false)}>Om mig</a>
                 </div>
 
-                <div className="nav-cta">
+                <div className={`nav-cta ${menuOpen ? "active" : ""}`} >
                     <a href="#contact">Kontakt</a>
+                </div>
+
+                <div
+                    className={`burger ${menuOpen ? "open" : ""}`}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+
                 </div>
             
             </div>
