dfe05666783aa4bfc3aea56156af6c3123b934c4
BlablaTara <tara0561@stud.kea.dk>
2026-04-09 23:31:37 +0200

    added animations for dots

--- src/components/heroComponents/Dots.js
+++ src/components/heroComponents/Dots.js
@@ -1,20 +1,44 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const dots = [
+    { text: "Frontend", x: "36%", y: "16%", size: 4 },
+    { text: "Design", x: "30%", y: "60%", size: 4  },
+    { text: "UI/UX", x: "52%", y: "37%", size: 7  },
+    { text: "React", x: "70%", y: "70%", size: 6  },
+    { text: "Next.js", x: "85%", y: "40%", size: 4  },
+    { text: "Startup", x: "60%", y: "10%", size: 5  },
+    { text: "Blabla", x: "48%", y: "75%", size: 6  },
+];
+
+
 export default function Dots() {
-    const dots = [
-        { text: "Frontend", x: "36%", y: "16%", size: 4 },
-        { text: "Design", x: "30%", y: "60%", size: 4  },
-        { text: "UI/UX", x: "52%", y: "37%", size: 7  },
-        { text: "React", x: "70%", y: "70%", size: 6  },
-        { text: "Next.js", x: "85%", y: "40%", size: 4  },
-        { text: "Startup", x: "60%", y: "10%", size: 5  },
-        { text: "Blabla", x: "48%", y: "75%", size: 6  },
-    ];
+
+    const [activeDots, setActiveDots] = useState([]);
+    const [lastIndex, setLastIndex] = useState(null);
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            let randomIndex;
+
+            do {
+                randomIndex = Math.floor(Math.random() * dots.length);
+            } while (randomIndex === lastIndex);
+
+            setLastIndex(randomIndex);
+            setActiveDots([randomIndex]);
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, [lastIndex]);
 
     return (
         <div className="dots">
             {dots.map((dot, index) => (
                 <div
                     key={index}
-                    className="dot"
+                    className={`dot ${activeDots.includes(index) ? "active" : ""}`}
                     style={{ 
                         left: dot.x, 
                         top: dot.y, 
@@ -22,7 +46,13 @@ export default function Dots() {
                         height: dot.size + "px"
                     }}
                 >
-                    <span className="dot-tooltip">{dot.text}</span>
+                                        <span
+                        className={`dot-tooltip ${
+                            activeDots.includes(index) ? "active" : ""
+                        }`}
+                    >
+                        {dot.text}
+                    </span>
                 </div>
             ))}
         </div>
