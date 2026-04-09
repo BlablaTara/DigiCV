9e3935c3f86885b941411f7efdc650af1ca526c6
BlablaTara <tara0561@stud.kea.dk>
2026-04-09 22:22:47 +0200

    dark styling for project-section

--- src/components/projectComponent/ProjectCard.js
+++ src/components/projectComponent/ProjectCard.js
@@ -3,19 +3,21 @@ import Image from "next/image";
 export default function ProjectCard({ title, description, image }) {
     return (
         <div className="project-card">
-            <div className="project-card-image">
-                <Image
-                src={image}
-                alt={title}
-                width={300}
-                height={200}
-                sizes="100vw"
-                style={{
-                    width: "100%",
-                    height: "200px",
-                    objectFit: "cover"
-                }}
-                />
+            <div className="face face1">
+                <div className="project-card-image" aria-hidden="true">
+                    <Image
+                    src={image}
+                    alt={title}
+                    width={300}
+                    height={200}
+                    sizes="100vw"
+                    style={{
+                        width: "100%",
+                        height: "200px",
+                        objectFit: "cover"
+                    }}
+                    />
+                </div>
             </div>
             <div className="project-card-content">
                 <h3>{title}</h3>
